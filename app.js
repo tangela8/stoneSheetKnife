6508e73adc22dc5c6a35030d5c88b3173305684c
tangela8 <61296102+tangela8@users.noreply.github.com>
2020-04-16 11:59:59 -0400

    added computer to js

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 alert(" Welcome to Stone, Sheet, Knife Game! ");
 
+//assign 1 = stone, 2 = sheet, 3 = knife for the computer to be chosen at random and will later use that to determine to who wins
+let computer = Math.floor(Math.random() * 3);
+if (computer <= 1) {
+    computer = "rock";
+    } else if(computer <= 2) {
+    computer = "paper";
+    } else {
+    computer = "scissors";
+    }
+
 //make a button to start the game
 function startGame() {
     //prompt the player to enter 1 of the 3 choices   
         const player = prompt("Type stone, sheet, or knife, if you dont spell it correctly, it will be undefined: ")
 }
 
-//assign 1 = rock, 2 = paper, 3 = scissors for the computer to be chosen at random and will later use that to determine to who wins
+
 //make a function to check the 3 choices following the rules of the game.
 //if it's the same choice then game is a tie
-//rock beats scissors, scissors beat paper, paper beat rock 
+//stone beats knife, knife slice sheet, sheet wraps stone
