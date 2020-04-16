bd4d334d02c5cd98122c0340858846a423a035e1
tangela8 <61296102+tangela8@users.noreply.github.com>
2020-04-16 14:24:39 -0400

    added checkwin function

--- app.js
+++ app.js
@@ -3,20 +3,33 @@ alert(" Welcome to Stone, Sheet, Knife Game! ");
 //assign 1 = stone, 2 = sheet, 3 = knife for the computer to be chosen at random and will later use that to determine to who wins
 let computer = Math.floor(Math.random() * 3);
 if (computer <= 1) {
-    computer = "rock";
+    computer = "stone";
     } else if(computer <= 2) {
-    computer = "paper";
+    computer = "sheet";
     } else {
-    computer = "scissors";
+    computer = "knife";
     }
 
 //make a button to start the game
 function startGame() {
     //prompt the player to enter 1 of the 3 choices   
-        const player = prompt("Type stone, sheet, or knife, if you dont spell it correctly, it will be undefined: ")
-}
+    const player = prompt("Type stone, sheet, or knife, if you dont spell it correctly, it will be undefined: ")
 
+    //make a function to check the 3 choices following the rules of the game.
+    //if it's the same choice then game is a tie
+    //stone beats knife, knife slice sheet, sheet wraps stone
+    const checkWin = function(computerChoice,playerChoice) {
+        if (computerChoice === playerChoice) {
+            return "Nobody wins, it's a tie! Refresh the page to play again!";
+        }
 
-//make a function to check the 3 choices following the rules of the game.
-//if it's the same choice then game is a tie
-//stone beats knife, knife slice sheet, sheet wraps stone
+        if (computerChoice === "stone" && playerChoice === "knife" || computerChoice === "sheet" && playerChoice === "stone" || computerChoice === "knife" && playerChoice === "sheet") {
+            return "You win. Refresh the page to play again!";
+        } else {
+            return "You lose. Refresh the page to play again!";
+        
+        }
+        
+    }
+}        
+
