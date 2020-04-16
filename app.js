f1b17285dd0b071027e825e0aa0b28b95fa5e179
tangela8 <61296102+tangela8@users.noreply.github.com>
2020-04-16 11:46:23 -0400

    added prompt and button

---
+++ app.js
@@ -0,0 +1,7 @@
+alert(" Welcome to Rock, Paper, Scissors Game! ");
+
+//make a button to start the game
+function startGame() {
+    //prompt the player to enter 1 of the 3 choices   
+        const player = prompt("Type rock, paper or scissors, if you dont spell it correctly, it will be undefined: ")
+}
