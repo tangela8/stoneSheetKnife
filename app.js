bdfccf648137f78e1e2b1f6424d4996a8d505a20
tangela8 <61296102+tangela8@users.noreply.github.com>
2020-04-16 11:48:56 -0400

    added pseudocode

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-alert(" Welcome to Rock, Paper, Scissors Game! ");
+alert(" Welcome to Stone, Sheet, Knife Game! ");
 
 //make a button to start the game
 function startGame() {
     //prompt the player to enter 1 of the 3 choices   
-        const player = prompt("Type rock, paper or scissors, if you dont spell it correctly, it will be undefined: ")
+        const player = prompt("Type stone, sheet, or knife, if you dont spell it correctly, it will be undefined: ")
 }
+
+//assign 1 = rock, 2 = paper, 3 = scissors for the computer to be chosen at random and will later use that to determine to who wins
+//make a function to check the 3 choices following the rules of the game.
+//if it's the same choice then game is a tie
+//rock beats scissors, scissors beat paper, paper beat rock 
