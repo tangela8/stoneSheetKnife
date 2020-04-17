17abf111d4cfdd4c97e55cdecebf4096337176ae
tangela8 <61296102+tangela8@users.noreply.github.com>
2020-04-17 11:53:51 -0400

    updated output

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ function startGame() {
     let playGame = checkWin(player,computer);
 
     //display the win/lose state of the game to the DOM
-    document.write("Player chooses " + player + " and " + "Computer chooses " + computer + " Sooooo </p>" + playGame);
+    document.write( '<h1>' + "Player chooses " + player + " and " + "Computer chooses " + computer + '<p>' + " Sooooo " + playGame + '</h1');
 
 }
 
